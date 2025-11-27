548c9ea11b78db21b0d1eb7f5b31b7536df9c524
jyotimishra6789 <riamishra639@gmail.com>
2025-11-27 11:04:02 +0530

    gif added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import SketchApp from "./components/SketchApp";
+import { Suspense } from "react";
+ const MainApp=React.lazy(()=>import("./components/SketchApp"));
 
 export default function App() {
+  const [loading,setLoading]=useState(true);
+  useEffect(()=>{
+    const timer=setTimeout(()=>{
+      setLoading(false)
+  },2000)
+  return()=>clearTimeout(timer);
+  },[]);
+  if(loading){
+    return(
+      <div style={{height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#ffffff",
+      }}><img 
+          src="/loader.gif" 
+          alt="loading animation" 
+          style={{ width: "120px", marginBottom: "15px" }}
+        />        <p style={{ fontSize: "20px", fontWeight: "600" }}>
+          Loading...
+        </p>
+</div>
+    );
+  }
   return (
     <div>
-      <SketchApp />
-    </div>
+      <Suspense fallback={<h2>loading..components..</h2>}>
+    <MainApp/>
+     </Suspense>
+     </div>
   );
 }
