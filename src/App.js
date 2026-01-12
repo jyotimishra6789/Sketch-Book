6676c73334ad2e39720dedc4035a0d684ff28d99
jyotimishra6789 <riamishra639@gmail.com>
2026-01-12 20:13:55 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
-import React, { Suspense, useState, useEffect } from "react";
-
-const MainApp = React.lazy(() => import("./components/SketchApp"));
+import SketchCanvas from "./components/SketchCanvas";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  
-
-  
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-
-    return () => clearTimeout(timer); // ✅ cleanup
-  }, []);
-
-  
-  if (loading) {
-    return (
-      <div
-        style={{
-          height: "100vh",
-          width: "100vw",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          background: "#ffffff",
-        }}
-      >
-        <img
-          src="https://media.tenor.com/3rLdVGILFeIAAAAi/drawing-sketch.gif"
-          alt="loading animation"
-          style={{ width: "120px", marginBottom: "15px" }}
-        />
-        <p style={{ fontSize: "20px", fontWeight: "600" }}>Loading...</p>
-      </div>
-    );
-  }
-
-  return (
-    <Suspense fallback={<h2>Loading components...</h2>}>
-      <MainApp />
-    </Suspense>
-  );
+  return <SketchCanvas />;
 }
